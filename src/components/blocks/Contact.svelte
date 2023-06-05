2d6b5fac667e733447b0a35621d063095e1927d2
silveltman <52583398+silveltman@users.noreply.github.com>
2023-06-05 13:41:54 +0200

    deel 2

--- src/components/blocks/Contact.svelte
+++ src/components/blocks/Contact.svelte
@@ -21,6 +21,9 @@
     phone: string
   }
 
+  export let content: any
+
+  // @robert verplaatsen naar content/pages/contact.md
   const suppliers: {
     [key: string]: SupplierData
   } = {
@@ -43,75 +46,72 @@
 
 <Section>
   <Container>
-    <Flow>
-      <Flow row>
-        <Flow>
-          <Highlight>
-            <Heading
-              as="h1"
-              text="Contact Anti-Slip Profiles"
-            />
-            <Text
-              text="Heeft u een vraag, opmerking of bericht voor Anti-Slip Profiles? Gebruikt u dan het onderstaande formulier. Anti-Slip Profiles neemt dan zo snel mogelijk contact met u op om uw vraag te beantwoorden."
-            />
-          </Highlight>
-          {#each [suppliers.ned, suppliers.scn] as supplier}
-            <img
-              src="/contact/{supplier.map}"
-              alt="map"
-            />
-          {/each}
-        </Flow>
-        <Flow>
-          <input
-            type="checkbox"
-            class="peer"
+    <Flow row>
+      <Flow>
+        <Highlight>
+          <Heading
+            as="h1"
+            text="Contact Anti-Slip Profiles"
           />
-          {#each [suppliers.ned, suppliers.scn] as supplier, i}
-            <Panel
-              class={i === 1
-                ? 'hidden peer-checked:block'
-                : 'block peer-checked:hidden'}
-              >{supplier.adres}
-            </Panel>
-            <Panel
-              class={i === 1
-                ? 'hidden peer-checked:block'
-                : 'block peer-checked:hidden'}
-            >
-              <Form key={supplier.form_key}>
-                <Input
-                  label="naam"
-                  id="naam"
-                />
-                <Input
-                  label="Email"
-                  id="email"
-                  type="email"
-                />
-                <Input
-                  label="Telefoon"
-                  id="telefoon"
-                  type="tel"
-                />
-                <Textarea
-                  label="Bericht"
-                  id="bericht"
-                />
-                <Button
-                  variant="solid"
-                  type="submit"
-                  text="Verstuur"
-                  class="light-orange"
-                />
-              </Form>
-            </Panel>
-          {/each}
-        </Flow>
+          <Text
+            text="Heeft u een vraag, opmerking of bericht voor Anti-Slip Profiles? Gebruikt u dan het onderstaande formulier. Anti-Slip Profiles neemt dan zo snel mogelijk contact met u op om uw vraag te beantwoorden."
+          />
+        </Highlight>
+        {#each [suppliers.ned, suppliers.scn] as supplier}
+          <img
+            src="/contact/{supplier.map}"
+            alt="map"
+          />
+        {/each}
+      </Flow>
+      <Flow>
+        <input
+          type="checkbox"
+          class="peer"
+        />
+        {#each [suppliers.ned, suppliers.scn] as supplier, i}
+          <Panel
+            class={i === 1
+              ? 'hidden peer-checked:block'
+              : 'block peer-checked:hidden'}
+          >
+            <!-- @robert aanvullen met contactgegevens -->
+            {supplier.adres}
+          </Panel>
+          <Panel
+            class={i === 1
+              ? 'hidden peer-checked:block'
+              : 'block peer-checked:hidden'}
+          >
+            <Form key={supplier.form_key}>
+              <Input
+                label="naam"
+                id="naam"
+              />
+              <Input
+                label="Email"
+                id="email"
+                type="email"
+              />
+              <Input
+                label="Telefoon"
+                id="telefoon"
+                type="tel"
+              />
+              <Textarea
+                label="Bericht"
+                id="bericht"
+              />
+              <Button
+                variant="solid"
+                type="submit"
+                text="Verstuur"
+                class="light-orange"
+              />
+            </Form>
+          </Panel>
+        {/each}
       </Flow>
     </Flow>
   </Container>
 </Section>
-
-<style>
-</style>
