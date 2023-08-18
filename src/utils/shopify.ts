d12c94884c15d1ee33a592fe1a22ae985aa0bd9c
silveltman <silveltman@gmail.com>
2023-08-18 13:53:54 +0200

    c

--- src/utils/shopify.ts
+++ src/utils/shopify.ts
@@ -66,7 +66,7 @@ async function createCart(merchandiseId: string, quantity: number) {
 }
 
 export async function addToCart(merchandiseId: string, quantity: number) {
-  rudderanalytics.track('AddToCart')
+  rudderanalytics.track('Product Added')
   const cartId = localStorage.cartId
   const isValid = cartId ? cartId.startsWith('gid://shopify/Cart/') : false
   if (isValid) {
